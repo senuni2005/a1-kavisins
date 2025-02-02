7247f4d825d4d90fe8a1366c621875fbc33bae36
Senuni Kavisinghe <senuni.05@gmail.com>
2025-02-01 21:49:43 -0500

    closes #4: Update main with logger

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,7 +11,7 @@ import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// MVP for Maze Runner: Handles single commands for maze movements with -i <file path> support.
+// MVP for Maze Runner: Handle single commands for maze movements with -i <file path> support.
 
 public class Main {
     private static final Logger logger = LogManager.getLogger();
